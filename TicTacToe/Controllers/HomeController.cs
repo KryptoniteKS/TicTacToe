a042c6382c71e7654564d324ea52af895010ea4c
Douglas B <puddingoo343@gmail.com>
2022-12-12 22:43:51 -0500

    Add project files.

---
+++ TicTacToe/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicTacToe.Models;
+
+namespace TicTacToe.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Simply returns our Setup.cshtml page, which is our default home page configured in RouteConfig.cs
+        public ActionResult Setup()
+        {
+            return View();
+        }
+
+        // Constructor for Game view that takes in a parameter from Setup.cshtml for board dimensions
+        public ActionResult Game(int? dimension)
+        {
+            // Check to see if a null type was sent over. If this is the case, default us to 3.
+            if (!dimension.HasValue)
+            {
+                dimension = 3;
+            }
+            else if (dimension > 11)
+            {
+                dimension = 11;
+            }
+
+            // Sets the dimensions of the game board and returns it to the Game view to render. This value is grabbed from the Setup.cshtml page
+            var board = new Board() { Dimension = (int)dimension };
+            return View(board);
+        }
+
+    }
+}
---
+++ TicTacToe/Models/Board.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicTacToe.Models
+{
+    public class Board
+    {
+        public int Dimension { get; set; } // The dimensions of the board, i.e., 5 means a 5x5 board
+        public int PanelID { get; set; } = 0; // The ID Given to the buttons that represent the board
+    }
+}
